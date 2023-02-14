3719eb1dd72e40c2d6dee58581f6cfa93dd3a3f3
MDsithick <sithickbilal21@gmail.com>
2023-02-14 12:20:23 +0530

    firstcommit

---
+++ script.js
@@ -0,0 +1,35 @@
+setTimeout(function() {
+    document.getElementById("countdown").innerHTML = "10";
+    setTimeout(function() {
+      document.getElementById("countdown").innerHTML = "9";
+      setTimeout(function() {
+        document.getElementById("countdown").innerHTML = "8";
+        setTimeout(function() {
+          document.getElementById("countdown").innerHTML = "7";
+          setTimeout(function() {
+            document.getElementById("countdown").innerHTML = "6";
+            setTimeout(function() {
+              document.getElementById("countdown").innerHTML = "5";
+              setTimeout(function() {
+                document.getElementById("countdown").innerHTML = "4";
+                setTimeout(function() {
+                  document.getElementById("countdown").innerHTML = "3";
+                  setTimeout(function() {
+                    document.getElementById("countdown").innerHTML = "2";
+                    setTimeout(function() {
+                      document.getElementById("countdown").innerHTML = "1";
+                      setTimeout(function() {
+                        document.getElementById("countdown").style.display = "none";
+                        document.getElementById("message").style.display = "block";
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+  
